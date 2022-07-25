0a22de7d9ddbc17754900e73c728b49c39abe027
Dudsdudy <tiago-de-santana@hotmail.com>
2022-07-25 20:23:25 -0300

    first object

---
+++ Programação Orienta a Objeto POO/RodarAPP.java
@@ -0,0 +1,24 @@
+class RodaApp {
+
+    public static void main(String[] args) {
+        
+   
+        
+    
+
+    Carro carro1 = new Carro();
+
+    carro1.setCor("Azul");
+    carro1.setModelo("A3");
+    carro1.setcapacidadeTanque(59);
+
+    System.out.println(carro1.getModelo());
+    System.out.println(carro1.getCor());
+    System.out.println(carro1.getcapacidadeTanque());
+    
+
+}
+
+}
+
+
--- Programação Orienta a Objeto POO/helloworld.java
+++ Programação Orienta a Objeto POO/helloworld.java
@@ -1,11 +1,55 @@
 class Carro {
-    Carro ()
-    string cor;
-    string modelo;
-    int capacidadeTanque;
-    void frear (){
+
+    String cor;
+    String modelo;
+    Int capacidadeTanque;
+
+    Carro(){
 
     }
 
+    Carro (String cor, String modelo, Int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque; 
+    }
+    
+
+//Construtor aqui
+
+void setCor(String cor){
+    this.cor;
+}
+
+void getCor(){
+    return cor;
+}
+
+void setModelo(String modelo){
+    this.modelo = modelo;
+}
+
+void getModelo(){
+    return modelo;
+}
+
+void setcapacidadeTanque(int capacidadeTanque){
+    this.capacidadeTanque = capacidadeTanque
+}
+
+int getcapacidadeTanque(){
+    return capacidadeTanque
+}
+
+
+// get e set tanque
+
+
+// metodo do total para encher o tanque
+
+
+double totalValorTanque (double valorCombustivel){
+    return capacidadeTanque * valorCombustivel;
+}
 
 }
